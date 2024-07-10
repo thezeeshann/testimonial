a8c47968188de3601bb85890fbf7176da80f8768
thezeeshann <info.zeeshann@gmail.com>
2024-07-11 00:19:15 +0530

    feat: Added next-auth authenticatinon

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,16 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import db from "./db";
 
-
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-})
+  secret: process.env.AUTH_SECRET,
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+});
