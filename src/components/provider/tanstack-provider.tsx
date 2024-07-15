f326b2a1b07c09a38bab9de719d4e5ca2d1b57e2
thezeeshann <info.zeeshann@gmail.com>
2024-07-15 21:54:33 +0530

    feat: Setup theme & tanstack provider

--- src/components/provider/tanstack-provider.tsx
+++ src/components/provider/tanstack-provider.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode } from "react";
 
 const queryClient = new QueryClient();
 
 const TanstackProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
