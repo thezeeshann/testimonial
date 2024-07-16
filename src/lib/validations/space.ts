029dd4959cffb293b2d0fa0f2724d04c00fd0836
thezeeshann <info.zeeshann@gmail.com>
2024-07-16 23:33:10 +0530

    feat: Added space schema

---
+++ src/lib/validations/space.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const themeEnum = z.enum(["Light", "Dark"]);
+
+export const spaceSchema = z.object({
+  name: z.string().min(4, {
+    message: "Name must be at least 4 characters logn",
+  }),
+  logo: z.string().optional(),
+  title: z.string().min(4, {
+    message: "Title must be at least 4 characters logn",
+  }),
+  message: z.string().min(6, {
+    message: "Message must be at least 6 characters logn",
+  }),
+  rating: z.boolean(),
+  theme: themeEnum,
+  questionOne: z.string().min(6, {
+    message: "Question must be at least 6 characters logn",
+  }),
+  questionTwo: z.string().min(6, {
+    message: "Question must be at least 6 characters logn",
+  }),
+  questionThree: z.string().min(6, {
+    message: "Question must be at least 6 characters logn",
+  }),
+});
