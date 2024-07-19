f7a4ec3c4478c21e2fb469d9218a076edaffcf8a
thezeeshann <info.zeeshann@gmail.com>
2024-07-19 23:16:14 +0530

    updated

--- src/components/provider/tanstack-provider.tsx
+++ src/components/provider/tanstack-provider.tsx
@@ -2,11 +2,13 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
-const queryClient = new QueryClient();
 
 const TanstackProvider = ({ children }: { children: ReactNode }) => {
+
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
