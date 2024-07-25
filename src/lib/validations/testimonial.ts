c55bce48da3218ae3ea1429a1f22206c37243b0c
thezeeshann <info.zeeshann@gmail.com>
2024-07-25 22:37:59 +0530

    feat: Added testimonial zod schema

---
+++ src/lib/validations/testimonial.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const testimonialSchema = z.object({
+  name: z.string().min(4, {
+    message: "Name must be at least 4 characters logn",
+  }),
+  message: z.string().min(6, {
+    message: "Message must be at least 6 characters logn",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  image: z.string(),
+  rating: z.number().min(1).max(5),
+  photo: z.string(),
+  permission: z.boolean().default(false),
+});
