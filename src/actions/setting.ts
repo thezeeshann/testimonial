e37ec548a2cbcd29e7070f4efa12c75e17df5ac8
thezeeshann <info.zeeshann@gmail.com>
2024-08-07 23:38:06 +0530

    feat: Added settings schema & actions

---
+++ src/actions/setting.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { settingsSchema } from "@/lib/validations/setting";
+import { currentUser } from "@/lib/user";
+import { z } from "zod";
+import db from "@/lib/db";
+
+export const setting = async (values: z.infer<typeof settingsSchema>) => {
+  try {
+    const validatedFields = settingsSchema.safeParse(values);
+    const { name, image } = validatedFields.data;
+    if (!validatedFields.success) {
+      return {
+        error: "Invalid fields!",
+      };
+    }
+    const user = await currentUser();
+    if (!user) {
+      return {
+        error: "Unauthorized",
+      };
+    }
+
+    await db.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        name: name,
+        image: image,
+      },
+    });
+
+    return {
+      success: "Setting updated",
+    };
+  } catch (error) {
+    return {
+      error: "Something went wrong",
+    };
+  }
+};
--- src/lib/validations/setting.ts
+++ src/lib/validations/setting.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 
 export const settingsSchema = z.object({
-  name: z.optional(z.string()),
-  image: z.optional(z.string()),
+  name: z
+    .string()
+    .min(4, {
+      message: "Name must be at least 4 characters logn",
+    })
+    .optional(),
+  image: z.string().optional(),
 });
