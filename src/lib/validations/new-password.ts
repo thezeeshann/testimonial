88919934823252a1d7c9dff044870c4010cb5050
thezeeshann <info.zeeshann@gmail.com>
2024-08-20 00:03:14 +0530

    feat: Added forgot password schema

---
+++ src/lib/validations/new-password.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const newPasswordSchema = z.object({
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long",
+  }),
+  token: z.string().nullable().optional(),
+});
