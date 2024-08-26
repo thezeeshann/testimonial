6a1e76759b3d4e9cb4e7ef0783c90bfc7d1bf7e7
thezeeshann <info.zeeshann@gmail.com>
2024-08-26 23:44:49 +0530

    feat: Added testimonial script

---
+++ src/components/dashboard/generate-script.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+const GenerateScript = () => {
+  return (
+    <div className="mt-5 0">
+      <pre className="text-[13px] p-[1rem] overflow-x-scroll  bg-[#1E1E1E] leading-3">
+        <code className="text-[#9CDCFE]">
+          <span className="text-[#D4D4D4]">&lt;</span>
+          <span className="text-[#9CDCFE]">script type</span>
+          <span className="text-[#9CDCFE]">=</span>
+          <span className="text-[#CE9178]">&quot;text/javascript&quot;</span>
+          <span className=""> src</span>
+          <span className="">=</span>
+          <span className="text-[#CE9178]">
+            &quot;https://testimonial.to/js/iframeResizer.min.js&quot;
+          </span>
+          <span className="text-[#D4D4D4]">&gt;</span>
+          <span className="text-[#D4D4D4]">&lt;/</span>
+          <span className="text-[#9CDCFE]">script</span>
+          <span className="text-[#D4D4D4]">&gt;</span>
+          <div className="-my-1">
+            <br />
+          </div>
+          <span className="text-[#D4D4D4]">&lt;</span>
+          <span>iframe id</span>
+          <span className="text-[#D4D4D4]">=</span>
+          <span className="text-[#CE9178]">
+            &apos;testimonialto-stuent-reviews-tag-all-light&apos;
+          </span>
+          <span> src</span>
+          <span className="text-[#D4D4D4]">=</span>
+          <span className="text-[#CE9178]">
+            &quot;https://embed-v2.testimonial.to/w/stuent-reviews?theme=light&amp;card=base&amp;loadMore=on&amp;initialCount=20&amp;tag=all&quot;
+          </span>
+          <span> frameborder</span>
+          <span className="text-[#D4D4D4]">=</span>
+          <span className="text-[#CE9178]">&quot;0&quot;</span>
+          <span> scrolling</span>
+          <span className="text-[#D4D4D4]">=</span>
+          <span className="text-[#CE9178]">&quot;no&quot;</span>
+          <span> width</span>
+          <span className="text-[#D4D4D4]">=</span>
+          <span className="text-[#CE9178]">&quot;100%&quot;</span>
+          <span className="text-[#D4D4D4]">&gt;</span>
+          <span className="text-[#D4D4D4]">&lt;</span>
+          <span className="text-[#D4D4D4]">/</span>
+          <span>iframe</span>
+          <span className="text-[#D4D4D4]">&gt;</span>
+          <div className="-my-1">
+            <br />
+          </div>
+          <span className="text-[#D4D4D4]">&lt;</span>
+          <span className="text-[#9CDCFE]">script type</span>
+          <span className="text-[#9CDCFE]">=</span>
+          <span className="text-[#CE9178]">&quot;text/javascript&quot;</span>
+          <span className="text-[#D4D4D4]">&gt;</span>
+          <span className="text-[#DCDCAA]">iFrameResize</span>
+          {/* <span className="text-[#D4D4D4]">(</span>
+            <span className="text-[#D4D4D4]">{</span> */}
+          <span>log</span>
+          <span className="text-[#D4D4D4]">:</span>
+          <span className="text-[#569CD6]">false</span>
+          <span className="text-[#D4D4D4]">,</span>
+          <span className="text-[#D4D4D4]"></span>
+          <span className="text-[#569CD6]">false</span>
+          <span className="text-[#D4D4D4]">== $0</span>
+          <span className="text-[#D4D4D4]">,</span>
+          <span className="text-[#CE9178]">
+          &apos;#testimonialto-stuent-reviews-tag-all-light&apos;
+          </span>
+          <span className="text-[#D4D4D4]"></span>
+          <span className="text-[#D4D4D4]">:</span>
+          <span className="text-[#569CD6]">false</span>
+          <span className="text-[#D4D4D4]">,</span>
+          <span className="text-[#D4D4D4]"></span>
+          <span className="text-[#D4D4D4]">:</span>
+          <span className="text-[#D4D4D4]">:</span>
+          <span className="text-[#D4D4D4]">:</span>
+          <span className="text-[#D4D4D4]">script</span>
+          <span className="text-[#D4D4D4]"></span>
+          <span className="text-[#D4D4D4]"></span>
+          <span className="text-[#D4D4D4]"></span>
+
+          <span className="text-[#D4D4D4]">&lt;/</span>
+          <span className="text-[#9CDCFE]">script</span>
+          <span className="text-[#D4D4D4]">&gt;</span>
+        </code>
+      </pre>
+    </div>
+  );
+};
+
+export default GenerateScript;
