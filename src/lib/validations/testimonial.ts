c3dbe701622461891142325100e3f7ffb27eeb1f
thezeeshann <info.zeeshann@gmail.com>
2024-09-01 23:19:41 +0530

    refactor: update testimonial schema

--- src/lib/validations/testimonial.ts
+++ src/lib/validations/testimonial.ts
@@ -4,14 +4,14 @@ export const testimonialSchema = z.object({
   name: z.string().min(4, {
     message: "Name must be at least 4 characters logn",
   }),
-  message: z.string().min(6, {
-    message: "Message must be at least 6 characters logn",
+  message: z.string().min(20, {
+    message: "Message must be at least 20 characters logn",
   }),
   email: z.string().email({
     message: "Please enter a valid email address",
   }),
-  image: z.string().optional(),
   rating: z.number().min(1).max(5),
+  image: z.string().optional(),
   photo: z.string().optional(),
   permission: z.boolean().default(false),
   spaceId: z.string(),
